036df0565c90c33515de41809c0ca8826ee52f29
SPreo26 <SPreo26@users.noreply.github.com>
2016-11-29 07:09:50 -0800

    finished assignment

---
+++ angular_webstore_files/helper.js
@@ -0,0 +1,20 @@
+var filter = function(){
+  var scope = angular.element(document.getElementById('productsCtrl')).scope();
+    scope.$apply(function () {
+      scope.filterUnderAmount(20);
+    });
+}
+
+$('document').ready(function(){
+  //vertical alignment for switch
+  $('.pull-down-switch').each(function() {
+    var $this=$(this);
+    $this.css('margin-top', $this.parent().height()-$this.height()-15)
+  });
+  //vert. align. for buttons
+  $('.pull-down-buttons').each(function() {
+    var $this=$(this);
+    $this.css('margin-top', $this.parent().height()-$this.height()-3)
+  });
+  $("[name='filter-checkbox']").bootstrapSwitch({offText:'Show all under $20', onText:'Show all', handleWidth:'150px', onColor:'success', offColor:'info', onSwitchChange:filter});
+});
---
+++ angular_webstore_files/products_ctrl.js
@@ -0,0 +1,96 @@
+(function() {
+  "use strict";
+
+  var app = angular.module("app", []);
+
+  app.controller("productsCtrl", function($scope, $http) {
+
+    $scope.setup = function() {
+      $http.get("https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js").then(function(response){
+        $scope.productData = response.data;
+        $scope.allProducts = $scope.productData.products;
+        $scope.visibleProducts = $scope.allProducts;
+        $scope.pageSettings = {cols: 3};
+        $scope.setupProducts();
+        $scope.setupPage();
+
+      },
+      function(error){
+        console.log(error.data);
+      })
+    }
+
+    $scope.setupPage = function() {
+      $('head>title').text($scope.productData.pageTitle);
+      $('.navbar-fixed-top p.navbar-brand').text($scope.productData.pageTitle);
+      $('body>footer .navbar-brand').text($scope.productData.extraInfo);
+    }
+
+    $scope.setupProducts = function() {
+      $scope.numProducts = $scope.allProducts.length;
+      $scope.numVisProducts = $scope.visibleProducts.length;
+      //set up products data to be used with 3 column list
+      $scope.products = []
+      var row=0, col=0, productIndex=0;
+      while (productIndex < $scope.numVisProducts){
+        $scope.products[row]=[];
+        while (col<$scope.pageSettings.cols){
+          if (productIndex >= $scope.numVisProducts){
+            break;
+          }
+          var product = $scope.visibleProducts[productIndex];
+          $scope.products[row].push({name:product.name, 
+           image:'http:'+product.mainImage.ref,
+           price:$scope.priceString(product.msrpInCents)});
+          col++, productIndex++;
+        }
+        row++;
+        col=0;
+      }
+    }
+
+    $scope.priceString = function(cents){
+      return '$'+cents/100;
+    }
+
+    $scope.filterUnderAmount = function(amount){
+      $scope.visibleProducts=[];
+      var state = $('input[name="filter-checkbox"]').bootstrapSwitch('state')
+      if(state){
+
+        var productIndex=0;
+        while (productIndex < $scope.numProducts){
+          var product = $scope.allProducts[productIndex];
+          var price = product.msrpInCents/100;
+          if (price<=amount){
+            $scope.visibleProducts.push(product);
+          }
+          productIndex++;
+        }
+      }
+      else{
+        $scope.visibleProducts=$scope.allProducts;
+      }
+
+      $scope.setupProducts();//must setup products again after filter applied
+    }
+
+    $scope.sortBy = function(criteria){
+      $scope.compareProp = criteria;
+      $scope.visibleProducts.sort($scope.compare);
+      $scope.setupProducts();//must setup products again after sort applied
+    }
+
+    $scope.compare = function compare(a,b) {
+      if (a[$scope.compareProp] < b[$scope.compareProp])
+        return -1;
+      if (a[$scope.compareProp] > b[$scope.compareProp])
+        return 1;
+      return 0;
+    }
+
+    window.$scope = $scope;
+});
+
+
+}());
